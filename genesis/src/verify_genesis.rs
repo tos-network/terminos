4a910b9bfdd45e6a7d20b0f21da2e4eaf8a0c2d5
Tomi Setsu <xuefubaby@hotmail.com>
2025-07-13 11:47:05 +0900

    verify genesis
    2025.7.13 verify genesis

--- genesis/src/verify_genesis.rs
+++ genesis/src/verify_genesis.rs
@@ -1,10 +1,11 @@
 use terminos_common::{
     block::Block,
     serializer::Serializer,
+    crypto::Hashable,
 };
 
 fn main() {
-    let genesis_hex = "02000000000000000000000197ff5ed3da00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000043fa8495c7a031f2c7a68c602eaa36d5a744fa69e44822f6b7e13f5cc2a7410";
+    let genesis_hex = "02000000000000000000000197ff69f08100000000000000000000000000000000000000000000000000000000000000000000000000000000000000043fa8495c7a031f2c7a68c602eaa36d5a744fa69e44822f6b7e13f5cc2a7410";
     
     println!("Genesis hex length: {}", genesis_hex.len());
     println!("Genesis hex: {}", genesis_hex);
@@ -31,6 +32,16 @@ fn main() {
             println!("Block parsed successfully!");
             println!("Block version: {:?}", block.get_version());
             println!("Block height: {}", block.get_height());
+            println!("Block timestamp: {}", block.get_timestamp());
+            println!("Block nonce: {}", block.get_nonce());
+            println!("Block extra nonce: {}", hex::encode(block.get_extra_nonce()));
+            println!("Block miner public key: {}", block.get_miner().to_hex());
+            println!("Block tips count: {}", block.get_tips().len());
+            println!("Block tips hash: {}", block.get_header().get_tips_hash());
+            println!("Block transactions count: {}", block.get_txs_count());
+            println!("Block transactions hash: {}", block.get_header().get_txs_hash());
+            println!("Block work hash: {}", block.get_header().get_work_hash());
+            println!("Block POW hash (V2): {:?}", block.get_pow_hash(terminos_common::block::Algorithm::V2));
             println!("Block hash: {}", block.hash());
         },
         Err(e) => {
